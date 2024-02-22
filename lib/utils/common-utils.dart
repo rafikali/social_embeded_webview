2f3de4372bb9eb7c75a9c7752b3ad61227b59592
rafikali7 <rafik@sociair.com>
2024-02-22 12:50:24 +0545

    changed type string to uri after facing an casting issue

--- lib/utils/common-utils.dart
+++ lib/utils/common-utils.dart
@@ -2,10 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
-String htmlToURI(String code) {
+Uri htmlToURI(String code) {
   return Uri.dataFromString(code,
-          mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
-      .toString();
+      mimeType: 'text/html', encoding: Encoding.getByName('utf-8'));
 }
 
 String colorToHtmlRGBA(Color c) {
